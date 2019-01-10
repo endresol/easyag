5fcae3a0d884348176e643aa41e652ea3103a2ef
Endre Solem <esolem@gmail.com>
2019-01-10 13:07:39 +0100

    adding register mockup service and som register gui using service

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2> Register </h2>
+
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="e-mail"
+    />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="Password"
+    />
+    <br />
+    <button
+      @click='register'>
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password,
+      })
+      console.log(response.data);
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
